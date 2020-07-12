5c7b627c9c332976b2352196685905379211d93f
Ahana <ahanasi@seas.upenn.edu>
2020-07-12 16:43:10 -0700

    Increase move range to include diagonal capture

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,7 +2,7 @@ require_relative "piece.rb"
 
 class Pawn < Piece
   def initialize(color)
-    @move_range = [[0, 1]]
+    @move_range = [[0, 1], [1,1], [-1,1]]
     @icon = (color == "white") ? "♟︎" : "♙"
     super
   end
