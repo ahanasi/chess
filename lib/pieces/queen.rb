04b5bf9da00c52a31512e7d0c7b3b8437ca915fb
Ahana <ahanasi@seas.upenn.edu>
2020-07-12 15:42:34 -0700

    Remove reference to SlidingPiece module

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,7 +2,6 @@ require_relative "piece.rb"
 require_relative "sliding.rb"
 
 class Queen < Piece
-  include SlidingPiece
 
   def initialize(color)
     @move_range = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
