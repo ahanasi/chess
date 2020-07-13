d41238f4f13c0923a2dde727ac7be0fdb951ef0d
Ahana <ahanasi@seas.upenn.edu>
2020-07-12 18:27:39 -0700

    Include two-step in move range

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,7 +2,7 @@ require_relative "piece.rb"
 
 class Pawn < Piece
   def initialize(color)
-    @move_range = [[1,0], [1,1], [1,-1]]
+    @move_range = [[1,0], [1,1], [1,-1], [2,0]]
     @icon = (color == "white") ? "♟︎" : "♙"
     super
   end
