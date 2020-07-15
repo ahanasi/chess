351adeac1efb70807fde033584a235f6a47da1ea
Ahana <ahanasi@seas.upenn.edu>
2020-07-14 18:52:00 -0700

    Include movement for black pawns

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,7 +2,7 @@ require_relative "piece.rb"
 
 class Pawn < Piece
   def initialize(color)
-    @move_range = [[1,0], [1,1], [1,-1], [2,0]]
+    @move_range = (color == "white") ? [[1,0], [1,1], [1,-1], [2,0]] : [[-1,0], [-1,1], [-1,-1], [-2,0]]
     @icon = (color == "white") ? "♟︎" : "♙"
     super
   end
