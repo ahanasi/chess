002670b81056157dc015e401101eb61571742783
Ahana <ahanasi@seas.upenn.edu>
2020-07-28 21:45:28 -0700

    Add cant_ep parameter

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,9 +1,11 @@
 require_relative "piece.rb"
 
 class Pawn < Piece
+  attr_accessor :cant_ep
   def initialize(color)
     @move_range = (color == "white") ? [[1,0], [1,1], [1,-1], [2,0]] : [[-1,0], [-1,1], [-1,-1], [-2,0]]
     @icon = (color == "white") ? "♟︎" : "♙"
+    @cant_ep = false
     super
   end
 end
